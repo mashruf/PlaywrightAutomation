9c9252e8f20defe331d8bd4bafc8ae13590c73ae
Mashruf Rohon <mashrufrohon@gmail.com>
2025-08-08 14:19:20 +0600

    browser context and page

--- tests/UIBasicsTest.spec.js
+++ tests/UIBasicsTest.spec.js
@@ -1,5 +1,15 @@
 const {test} = require('@playwright/test');
 
-test('First Playwright test',async ()=>{
+test('Browser Context Playwright test',async ({browser})=>{
+    const context = await browser.newContext();//open new instance
+    const page = await context.newPage();//on that instance open a page
+
+    //on that page go to the url
+    await page.goto("https://google.com");
+})
+
+test('Page Playwright test',async ({page})=>{
+    
+    await page.goto("https://google.com");
     
 })
