3eeef4571f813a1b717687be33d3448426aa8971
Mashruf Rohon <mashrufrohon@gmail.com>
2025-08-15 11:25:05 +0600

    Supported locators and typing into elements

--- tests/UIBasicsTest.spec.js
+++ tests/UIBasicsTest.spec.js
@@ -1,14 +1,18 @@
 const {test, expect} = require('@playwright/test');
 
-test('Browser Context Playwright test',async ({browser})=>{
+test.only('Browser Context Playwright test',async ({browser})=>{
     const context = await browser.newContext();//open new instance
     const page = await context.newPage();//on that instance open a page
 
     //on that page go to the url
-    await page.goto("https://google.com");
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    console.log(await page.title());
+    await page.locator('#username').fill("rahulshetty");
+    await page.locator('[name="password"]').fill("learning");
+    await page.locator("#signInBtn").click();
 })
 
-test.only('Page Playwright test',async ({page})=>{
+test('Page Playwright test',async ({page})=>{
     
     await page.goto("https://google.com");
     console.log(await page.title()); //printing title in console
