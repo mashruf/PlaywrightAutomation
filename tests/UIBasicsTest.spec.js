2707793a65dd3950453dd7bc36db47db1a728be0
Mashruf Rohon <mashrufrohon@gmail.com>
2025-08-17 18:14:40 +0600

    Extracting multiple web elements in page

--- tests/UIBasicsTest.spec.js
+++ tests/UIBasicsTest.spec.js
@@ -3,15 +3,26 @@ const {test, expect} = require('@playwright/test');
 test.only('Browser Context Playwright test',async ({browser})=>{
     const context = await browser.newContext();//open new instance
     const page = await context.newPage();//on that instance open a page
+    const userName = page.locator('#username'); 
+    const signIn = page.locator("#signInBtn");
 
     //on that page go to the url
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
     console.log(await page.title());
-    await page.locator('#username').fill("rahulshetty");
+    await userName.fill("rahulshetty"); //using the variable that hold the locator
     await page.locator('[name="password"]').fill("learning");
-    await page.locator("#signInBtn").click();
+    await signIn.click();
     console.log(await page.locator("[style*='block']").textContent());
     await expect(page.locator("[style*='block']")).toContainText("Incorrect username/password.");
+    
+    await userName.fill("");//will erase the previously entered username
+    await userName.fill("rahulshettyacademy");//using the varible that holds the locator
+    await signIn.click();//using the variable that hold the locator
+
+    console.log(await page.locator(".card-body a").first().textContent());//first product
+    console.log(await page.locator(".card-body a").nth(1).textContent());//second product
+    console.log(await page.locator(".card-body a").last().textContent());//last product
+
 
 })
 
